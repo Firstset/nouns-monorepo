d2c9c14aecaa3cae28bb8a97754cb8b8087898f8
Bernat <accounts@bernat.xyz>
2025-02-20 10:42:11 +0100

    add logs and correct quantized descriptors

--- packages/nouns-contracts/tasks/populate-descriptor.ts
+++ packages/nouns-contracts/tasks/populate-descriptor.ts
@@ -1,5 +1,5 @@
 import { task, types } from 'hardhat/config';
-import ImageData from '../files/image-data-v3.json';
+import ImageData from '../files/image-data-v3-quantized.json';
 import { dataToDescriptorInput } from './utils';
 
 task('populate-descriptor', 'Populates the descriptor with color palettes and Noun parts')
@@ -19,23 +19,36 @@ task('populate-descriptor', 'Populates the descriptor with color palettes and No
     //const options = { gasLimit: network.name === 'hardhat' ? 30000000 : undefined };
     const options = { gasLimit: network.name === 'hardhat' ? 30000000 : 30000000 };
 
+    console.log('Populating descriptor...');
+
     const descriptorFactory = await ethers.getContractFactory('NounsDescriptorV2', {
       libraries: {
         NFTDescriptorV2: nftDescriptor,
       },
     });
+
+    console.log('Descriptor factory created');
+
     const descriptorContract = descriptorFactory.attach(nounsDescriptor);
+
+    console.log('Descriptor contract attached');
 
     const { bgcolors, palette, images } = ImageData;
     const { bodies, accessories, heads, glasses } = images;
+
+    console.log('Image data extracted');
 
     const bodiesPage = dataToDescriptorInput(bodies.map(({ data }) => data));
     const headsPage = dataToDescriptorInput(heads.map(({ data }) => data));
     const glassesPage = dataToDescriptorInput(glasses.map(({ data }) => data));
     const accessoriesPage = dataToDescriptorInput(accessories.map(({ data }) => data));
 
+    console.log('Data to descriptor input extracted');
+
     // await descriptorContract.addManyBackgrounds(bgcolors);
     await descriptorContract.setPalette(0, `0x000000${palette.join('')}`);
+
+    console.log('Palette set');
 
     await descriptorContract.addBodies(
       bodiesPage.encodedCompressed,
